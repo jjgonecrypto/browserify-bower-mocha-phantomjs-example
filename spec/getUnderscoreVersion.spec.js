48281496d1aaa2d80c49d312af818001fe07c364
Justin J. Moses <justinjmoses@gmail.com>
2014-08-13 16:37:32 -0400

    Cleanup

--- spec/getUnderscoreVersion.spec.js
+++ spec/getUnderscoreVersion.spec.js
@@ -5,8 +5,8 @@ var expect = require('chai').expect;
 var getUnderscoreVersion = require('../lib/getUnderscoreVersion');
 
 describe('getUnderscoreVersion', function () {
-  it('must work', function () {
-    expect(typeof getUnderscoreVersion).to.equal('function');
-    expect(getUnderscoreVersion()).to.equal('1.6.0');
-  });
+    it('must work', function () {
+        expect(typeof getUnderscoreVersion).to.equal('function');
+        expect(getUnderscoreVersion()).to.equal('1.6.0');
+    });
 });
--- spec/getjQueryVersion.spec.js
+++ spec/getjQueryVersion.spec.js
@@ -5,8 +5,8 @@ var expect = require('chai').expect;
 var getjQueryVersion = require('../lib/getjQueryVersion');
 
 describe('getjQueryVersion', function () {
-  it('must work', function () {
-    expect(typeof getjQueryVersion).to.equal('function');
-    expect(getjQueryVersion()).to.equal('2.1.1');
-  });
+    it('must work', function () {
+        expect(typeof getjQueryVersion).to.equal('function');
+        expect(getjQueryVersion()).to.equal('2.1.1');
+    });
 });
